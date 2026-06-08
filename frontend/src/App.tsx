69067acfc9505ae6bbdd8fc118e4b94f8211d15f
kimse0ngjun <tjdwns8756@naver.com>
2026-06-08 09:39:28 +0900

    chore: 공통 스타일 및 화면 구조 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
-import CompanyList from "./pages/company/CompantList";
-import CompanyDetail from "./pages/company/CompanyDetail";
-import MaterialList from "./pages/material/MeterialList";
-import MaterialWrite from "./pages/material/MaterialWrite";
 
 function App() {
   return (
@@ -15,10 +11,6 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route element={<Layout />}>
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/company" element={<CompanyList />} />
-          <Route path="/company/:id" element={<CompanyDetail />} />
-          <Route path="/material" element={<MaterialList />} />
-          <Route path="/material/write" element={<MaterialWrite />} />
         </Route>
       </Routes>
     </BrowserRouter>
